dac8667f7af030ff4e8b2c0a8dedad6eed331da2
Mary Sophiya <mrsophiya@gmail.com>
2023-12-22 16:22:10 +0530

    training added

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -3,11 +3,12 @@ import os
 from cnnClassifier.utils.common import read_yaml,create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
-                                                PrepareCallbacksConfig)
+                                                PrepareCallbacksConfig,
+                                                TrainingConfig)
 
 
 class ConfigurationManager:
-      def __init__(
+        def __init__(
             self,
             config_filepath = CONFIG_FILE_PATH,
             params_filepath = PARAMS_FILE_PATH):
@@ -17,7 +18,7 @@ class ConfigurationManager:
 
             create_directories([self.config.artifacts_root])
 
-      def get_data_ingestion_config(self) -> DataIngestionConfig:
+        def get_data_ingestion_config(self) -> DataIngestionConfig:
           config = self.config.data_ingestion
 
           create_directories([config.root_dir])
@@ -30,12 +31,12 @@ class ConfigurationManager:
           )
 
           return data_ingestion_config
-      def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
-        config = self.config.prepare_base_model
+        def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+            config = self.config.prepare_base_model
         
-        create_directories([config.root_dir])
+            create_directories([config.root_dir])
 
-        prepare_base_model_config = PrepareBaseModelConfig(
+            prepare_base_model_config = PrepareBaseModelConfig(
             root_dir=Path(config.root_dir),
             base_model_path=Path(config.base_model_path),
             updated_base_model_path=Path(config.updated_base_model_path),
@@ -44,23 +45,44 @@ class ConfigurationManager:
             params_include_top=self.params.INCLUDE_TOP,
             params_weights=self.params.WEIGHTS,
             params_classes=self.params.CLASSES
-        )
+           )
 
-        return prepare_base_model_config
+            return prepare_base_model_config
       
       
-      def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
-        config = self.config.prepare_callbacks
-        model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
-        create_directories([
-            Path(model_ckpt_dir),
-            Path(config.tensorboard_root_log_dir)
-        ])
+        def get_prepare_callback_config(self) -> PrepareCallbacksConfig:
+            config = self.config.prepare_callbacks
+            model_ckpt_dir = os.path.dirname(config.checkpoint_model_filepath)
+            create_directories([
+                Path(model_ckpt_dir),
+                Path(config.tensorboard_root_log_dir)
+            ])
 
-        prepare_callback_config = PrepareCallbacksConfig(
-            root_dir=Path(config.root_dir),
-            tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
-            checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
-        )
+            prepare_callback_config = PrepareCallbacksConfig(
+                root_dir=Path(config.root_dir),
+                tensorboard_root_log_dir=Path(config.tensorboard_root_log_dir),
+                checkpoint_model_filepath=Path(config.checkpoint_model_filepath)
+            )
 
-        return prepare_callback_config
+            return prepare_callback_config
+        def get_training_config(self) -> TrainingConfig:
+            training = self.config.training
+            prepare_base_model = self.config.prepare_base_model
+            params = self.params
+            training_data = os.path.join(self.config.data_ingestion.unzip_dir, "Chicken-fecal-images")
+            create_directories([
+                Path(training.root_dir)
+            ])
+
+            training_config = TrainingConfig(
+                root_dir=Path(training.root_dir),
+                trained_model_path=Path(training.trained_model_path),
+                updated_base_model_path=Path(prepare_base_model.updated_base_model_path),
+                training_data=Path(training_data),
+                params_epochs=params.EPOCHS,
+                params_batch_size=params.BATCH_SIZE,
+                params_is_augmentation=params.AUGMENTATION,
+                params_image_size=params.IMAGE_SIZE
+                 )
+
+            return training_config
