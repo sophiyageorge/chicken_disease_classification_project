2c62d5298d5939c8a00dba1e152ed0b05dca40bb
Mary Sophiya <mrsophiya@gmail.com>
2023-12-31 10:11:40 +0530

    user app and prediction

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -4,7 +4,7 @@ from cnnClassifier.utils.common import read_yaml,create_directories
 from cnnClassifier.entity.config_entity import (DataIngestionConfig,
                                                 PrepareBaseModelConfig,
                                                 PrepareCallbacksConfig,
-                                                TrainingConfig)
+                                                TrainingConfig,EvaluationConfig)
 
 
 class ConfigurationManager:
@@ -33,19 +33,19 @@ class ConfigurationManager:
           return data_ingestion_config
         def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
             config = self.config.prepare_base_model
-        
+            
             create_directories([config.root_dir])
 
             prepare_base_model_config = PrepareBaseModelConfig(
-            root_dir=Path(config.root_dir),
-            base_model_path=Path(config.base_model_path),
-            updated_base_model_path=Path(config.updated_base_model_path),
-            params_image_size=self.params.IMAGE_SIZE,
-            params_learning_rate=self.params.LEARNING_RATE,
-            params_include_top=self.params.INCLUDE_TOP,
-            params_weights=self.params.WEIGHTS,
-            params_classes=self.params.CLASSES
-           )
+                root_dir=Path(config.root_dir),
+                base_model_path=Path(config.base_model_path),
+                updated_base_model_path=Path(config.updated_base_model_path),
+                params_image_size=self.params.IMAGE_SIZE,
+                params_learning_rate=self.params.LEARNING_RATE,
+                params_include_top=self.params.INCLUDE_TOP,
+                params_weights=self.params.WEIGHTS,
+                params_classes=self.params.CLASSES
+            )
 
             return prepare_base_model_config
       
@@ -86,3 +86,13 @@ class ConfigurationManager:
                  )
 
             return training_config
+        
+        def get_validation_config(self) -> EvaluationConfig:
+         eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+            )
+         return eval_config
